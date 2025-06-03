89e1a9e18a7ebd8a795a32480d15cf374dd5950e
vickyDevlo <vickymagare1996@gmail.com>
2025-06-03 14:28:59 +0530

    changes done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Routers } from "./Routers";
 
 function App() {
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-center bg-cover bg-no-repeat min-h-screen">
+    <div className="bg-[url('/bgImage.svg')] bg-center bg-cover bg-no-repeat min-h-screen">
       <Routers />
     </div>
   );
